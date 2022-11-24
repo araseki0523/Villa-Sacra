02ca3f1f04597ba2cfac02fd46d95ba5f178c063
C Oku <cloud.okuyama@gmail.com>
2022-11-24 10:52:16 +0900

    221124

---
+++ template/breadcrumb.php
@@ -0,0 +1,9 @@
+<nav class="breadcrumb layout-breadcrumb">
+    <div class="breadcrumb__inner">
+        <ol class="breadcrumb__list">
+            <?php if (function_exists('bcn_display')) {
+                bcn_display();
+            } ?>
+        </ol>
+    </div>
+</nav>
---
+++ template/gallery.php
@@ -0,0 +1,29 @@
+<section class="gallery layout-gallery">
+    <div class="gallery__inner">
+        <div class="gallery__title-area section-title">
+            <h2 class="gallery__title-ja section-title__ja">写真ギャラリー</h2>
+            <p class="gallery__title-en section-title__en">Photo galleries</p>
+        </div>
+        <div class="gallery__container">
+            <div class="swiper mySwiper-top">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery/gallery1.jpg" alt="ギャラリーの画像1">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery/gallery2.jpg" alt="ギャラリーの画像2">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery/gallery3.jpg" alt="ギャラリーの画像3">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery/gallery4.jpg" alt="ギャラリーの画像4">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="gallery__button-area">
+            <a href="" class="gallery__button button">MORE</a>
+        </div>
+    </div>
+</section>
