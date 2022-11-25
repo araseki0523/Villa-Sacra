596faa25ff415de3cb477b38d17b0571107d79f1
C Oku <cloud.okuyama@gmail.com>
2022-11-25 13:17:55 +0900

    221125

--- js/script.js
+++ js/script.js
@@ -25,8 +25,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   //ドロワーメニュー
   $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
     $(".js-drawer-open").toggleClass("open");
     $(".drawer-menu").toggleClass("open");
     $("html").toggleClass("is-fixed");
